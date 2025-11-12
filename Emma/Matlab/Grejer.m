e6b0753147272b9e1fce6e5f51ada91a2bb90a15
Emma Davidsson <davidsson470@gmail.com>
2025-11-12 10:21:17 +0100

    Skapat branch med mina grejor

---
+++ Emma/Matlab/Grejer.m
@@ -0,0 +1,51 @@
+clc; clear;
+%Test
+
+im = imread("Databas Lokal\Images\Images\DB1\DB1\);
+
+
+im: Image of unknown face, RGB-image in uint8 format in the range [0,255]
+
+id: The identity number (integer) of the identidied person, i.e. '1', '2', ..., '16 for the persons belonging to db1 and '0' for all other faces
+
+
+
+Translation
+Rotation
+Scaling
+Tone value
+Combinations
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
