6d639098949a9f98d6dfc4aa31d8a9ce2b223a9b
DubbGee <wilga619@student.liu.se>
2025-11-12 11:51:54 +0100

    Ycbcr eye map nu istället för RGB

--- TNM034_projekt.m
+++ TNM034_projekt.m
@@ -1,4 +1,4 @@
-A = imread("DB1\db1_01.jpg");
+A = imread("DB1\db1_05.jpg");
 A = im2double(A);
 
 % RGB kanaler
@@ -35,34 +35,40 @@ A_balanced = min(max(A_balanced, 0), 1);
 R = A_balanced(:,:,1);
 G = A_balanced(:,:,2);
 B = A_balanced(:,:,3);
+% luminans
+%Y = (R + G + B) / 3;
+YCbCr = rgb2ycbcr(A_balanced);
+Y  = YCbCr(:,:,1);
+Cb = YCbCr(:,:,2);
+Cr = YCbCr(:,:,3);
 
-% Y-komponent (luminans)
-Y = (R + G + B) / 3;
 
 % Eye Map C 
-EyeMapC = (1/3) * ((0.5*R.^2 + 0.15*G.^2 + 0.25*B.^2) ./ (Y.^2 + 0.01));
+%EyeMapC = (1/3) * ((0.5*R.^2 + 0.15*G.^2 + 0.25*B.^2) ./ (Y.^2 + 0.01));
+EyeMapC = (1/3) * (Cb.^2 + (1 - Cr).^2 + (Cb ./ (Cr + 0.01)));
+
 EyeMapC = mat2gray(EyeMapC); % normalisera mellan 0-1
+
 
 % Histogramnormalisering
 
 EyeMapC_eq = histeq(EyeMapC);
-EyeMapC_eq = 1 - EyeMapC_eq;
 
 % För att betona mörka områden mot ljusa delar
-se = strel('disk', 5);
-Y_dil = imdilate(Y, se);
-Y_ero = imerode(Y, se);
+se = strel('disk', 3);
+Y_dilate = imdilate(Y, se);
+Y_erode = imerode(Y, se);
 
-EyeMapL = Y_dil ./ (Y_ero + 0.001).^0.05;
-EyeMapL = (EyeMapL - min(EyeMapL(:))) / (max(EyeMapL(:)) - min(EyeMapL(:)) + eps);
+EyeMapL = Y_dilate ./ (Y_erode + 0.01);
+EyeMapL = mat2gray(EyeMapL);
 
 % Kombinera EyeMap C och L
 
 EyeMap = EyeMapC_eq .* EyeMapL;
-EyeMap = (EyeMap - min(EyeMap(:))) / (max(EyeMap(:)) - min(EyeMap(:)) + eps);
+EyeMap = mat2gray(EyeMap);
 
-
-threshold = 0.7;
+EyeMapCL = EyeMap;
+threshold = 0.5;
 [imSizerow,imSizecol] = size(EyeMap);
 for row = 1: imSizerow
 for col = 1: imSizecol
@@ -73,14 +79,16 @@ for col = 1: imSizecol
 end
 end
 kernel = strel('disk',1);
-kernel2 = strel('disk', 5);
+kernel2 = strel('disk', 6);
 EyeMap = imerode(EyeMap,kernel);
 EyeMap = imdilate(EyeMap,kernel2);
 
 % Visa resultat
 
 figure;
-subplot(2,2,1); imshow(A); title('Original');
-subplot(2,2,2); imshow(A_balanced); title('Gray World Balanced');
-subplot(2,2,3); imshow(EyeMapC_eq); title('Chroma Eye Map (C)');
-subplot(2,2,4); imshow(EyeMap); title('Combined Eye Map (C + L)');
+subplot(3,2,1); imshow(A); title('Original');
+subplot(3,2,2); imshow(A_balanced); title('Gray World Balanced');
+subplot(3,2,3); imshow(EyeMapC_eq); title('Chroma Eye Map (C)');
+subplot(3,2,4); imshow(EyeMapL); title('Luminance Eye Map (L)');
+subplot(3,2,5); imshow(EyeMapCL); title('Combined Eye Map (C + L)');
+subplot(3,2,6); imshow(EyeMap); title('Find Eyes');
