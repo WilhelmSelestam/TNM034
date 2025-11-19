de1544f50d541cf54b7ca38744d9aedc22fbdc14
Rickard Fuks <rickardfuksliu@gmail.com>
2025-11-19 15:50:47 +0100

    fixed face mask for some images

--- code/face_detection.m
+++ code/face_detection.m
@@ -1,6 +1,6 @@
 clc; clear; close all;
 
-I_orig = imread('db2/bl_04.jpg');
+I_orig = imread('DB1\db1_02.jpg');
 %I_orig = imrotate(I_orig, 10);
 %imwrite(I_orig, 'rot.jpg')
 I = im2double(I_orig);
@@ -12,7 +12,7 @@ thresh = quantile(Y(:), 0.95);
 ref_mask = Y > thresh;
 
 %Välj "White Patch" (1) eller "Grey World" (0)
-normalisera = 1;
+normalisera = 0;
 
 %Check för att se om bilden behöver vitbalanceras
 if nnz(ref_mask) > 100 
@@ -63,6 +63,7 @@ I_comp = im2uint8(I_comp);
 I_comp_ycbcr = rgb2ycbcr(I_comp);
 
 
+%{
 skin_mask_raw = detectSkin(I_comp_ycbcr);
 
 %imshow(skin_mask_raw);
@@ -76,10 +77,67 @@ skin_mask = imclose(skin_mask, se_close);
 
 skin_mask = imfill(skin_mask, 'holes');
 
-%skin_mask = bwconvhull(skin_mask);
+%skin_mask_full = bwconvhull(skin_mask);
 
 %imshow(skin_mask);
 
+%}
+
+newIm = I_comp;
+
+
+bink = mean(mean(mean(newIm)));
+
+%Få ut YCbCr & HSV
+imInYCbCr = rgb2ycbcr(newIm);
+imInHSV = rgb2hsv(newIm);
+
+newIm = imInYCbCr(:,:,1)./3 + imInYCbCr(:,:,2)./3 + imInYCbCr(:,:,3)./3;
+
+lpFilter= ones(5)/(5^2);
+
+newIm = imfilter(newIm,lpFilter,"symmetric");
+
+
+threshhold = bink + 5;
+% The thresholded image
+newIm = newIm < threshhold;
+
+
+%%%%%%%%%%%%%% Face Mask %%%%%%%%%%%%%%
+%Face Mask, Morphological operations
+
+innan = newIm;
+
+%imshow(innan);
+
+SE = strel('disk',10);
+newIm = imdilate(newIm,SE);
+
+
+% SE = strel('disk',3);
+% newIm = imdilate(newIm,SE);
+
+newIm = imfill(newIm,'holes');
+
+newIm =  newIm - innan;
+
+%imshow(newIm);
+
+SE = strel('disk',3);
+newIm = imclose(newIm,SE);
+
+SE = strel('disk',10);
+newIm = imopen(newIm,SE);
+
+newIm = imfill(newIm,'holes');
+
+skin_mask = newIm;
+
+imshow(skin_mask)
+
+
+%{
 figure;
 subplot(2, 1, 1);
 imshow(skin_mask_raw);
@@ -88,8 +146,9 @@ title('skin mask raw');
 subplot(2, 1, 2);
 imshow(skin_mask);
 title('skin mask');
-
+%}
 cc = bwconncomp(skin_mask);
+%cc = [cc cc2];
 stats = regionprops(cc, 'BoundingBox', 'Area');
 
 min_face_area = 500;
@@ -105,8 +164,8 @@ valid_pixel_lists = cc.PixelIdxList(valid_indices);
 
 detected_faces = [];
 face_scores = [];
-e1 = [];
-e2 = [];
+eye1 = [];
+eye2 = [];
 
 MIN_FACE_SCORE_THRESHOLD = 0.2; % ????????
 
@@ -115,19 +174,24 @@ for i = 1:length(face_candidates_stats)
     current_face_mask = false(size(skin_mask));
     current_face_mask(valid_pixel_lists{i}) = true;
 
+    current_face_mask = bwconvhull(current_face_mask);
+
     figure(i)
     imshow(current_face_mask)
     
     eyeMap = createEyeMap(I_comp_ycbcr, current_face_mask);
+   
     mouthMap = createMouthMap(I_comp_ycbcr, current_face_mask);
+
+     imshow(mouthMap)
     
     [score, ellipse, e1, e2] = verifyFaceCandidate(eyeMap, mouthMap, Y, current_face_mask);
     
     if score > MIN_FACE_SCORE_THRESHOLD
         detected_faces = [detected_faces; ellipse];
         face_scores = [face_scores; score];
-        e1 = e1;
-        e2 = e2;
+        eye1 = [eye1; e1];
+        eye2 = [eye2; e2];
     end
 end
 
@@ -147,7 +211,7 @@ detected_faces
 for i = 1:size(detected_faces, 1)
     %drawEllipse(detected_faces(i));
     drawEllipse(detected_faces(i, :));
-    line([e1(1), e2(1)], [e1(2), e2(2)], 'Color', 'yellow', 'LineWidth', 2);
+    line([eye1(1), eye2(1)], [eye1(2), eye2(2)], 'Color', 'yellow', 'LineWidth', 2);
 end
 
 
