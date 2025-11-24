7b1f1ca4fdc8fea42aa98518924fb17e87af399a
DubbGee <wilga619@student.liu.se>
2025-11-24 10:31:50 +0100

    ny funktionsfil EyeMap som gör chroma eyemap

---
+++ code/EyeMap.m
@@ -0,0 +1,56 @@
+function EyeMap = EyeMap(A_balanced)
+
+% Chroma Eye Map (Eye Map C)
+
+R = A_balanced(:,:,1);
+G = A_balanced(:,:,2);
+B = A_balanced(:,:,3);
+% luminans
+Yrgb = (R + G + B) / 3;
+YCbCr = rgb2ycbcr(A_balanced);
+Y  = YCbCr(:,:,1);
+Cb = YCbCr(:,:,2);
+Cr = YCbCr(:,:,3);
+
+
+% Eye Map C 
+%EyeMapC = (1/3) * ((0.5*R.^2 + 0.15*G.^2 + 0.25*B.^2) ./ (Yrgb.^2 + 0.01));
+EyeMapC = (1/3) * (Cb.^2 + (1 - Cr).^2 + (Cb ./ (Cr + 0.01)));
+
+EyeMapC = mat2gray(EyeMapC); % normalisera mellan 0-1
+
+
+% Histogramnormalisering
+
+EyeMapC_eq = histeq(EyeMapC);
+
+% För att betona mörka områden mot ljusa delar
+se = strel('disk', 3);
+Y_dilate = imdilate(Y, se);
+Y_erode = imerode(Y, se);
+
+EyeMapL = Y_dilate ./ (Y_erode + 0.01);
+EyeMapL = mat2gray(EyeMapL);
+
+% Kombinera EyeMap C och L
+
+EyeMap = EyeMapC_eq .* EyeMapL;
+EyeMap = mat2gray(EyeMap);
+
+EyeMapCL = EyeMap;
+threshold = 0.4;
+[imSizerow,imSizecol] = size(EyeMap);
+for row = 1: imSizerow
+for col = 1: imSizecol
+    if EyeMap(row,col) > threshold
+        EyeMap(row,col) = 1;
+    else EyeMap(row,col) = 0;
+    end
+end
+end
+kernel = strel('disk',1);
+kernel2 = strel('disk', 6);
+EyeMap = imerode(EyeMap,kernel);
+EyeMap = imdilate(EyeMap,kernel2);
+
+
--- code/TNM034_projekt_eye_map_eigenfaces.m
+++ code/TNM034_projekt_eye_map_eigenfaces.m
@@ -1,4 +1,4 @@
-A = imread("DB1\db1_12.jpg");
+A = imread("DB1\db1_10.jpg");
 A = im2double(A);
 
 % RGB kanaler
@@ -158,3 +158,4 @@ distances = vecnorm(weights - new_weights, 2, 1);
 %ef = reshape(eigenfaces(:,1), [h, w]);
 %figure; 
 %imshow(mat2gray(ef));
+%%
